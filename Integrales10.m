dd939d04a04d93f40b34ade03bde4749da67eb24
RamiroMatias717 <87044613+RamiroMatias717@users.noreply.github.com>
2021-07-06 17:52:03 -0500

    Update Integrales10.m

--- Integrales10.m
+++ Integrales10.m
@@ -1,24 +1,14 @@
-%Título:  TEORAMA FUNDAMENTAL DEL CALCULO 
-% Descripción: Script para la resolucion de ejercicios aplicando el teorama fundamental del calculo  
+%TÃ­tulo:  TEORAMA FUNDAMENTAL DEL CALCULO 
+% DescripciÃ³n: Script para la resolucion de ejercicios aplicando el teorama fundamental del calculo  
 % El tipo de funcion es  f= 2x^2+x^2-6
 % Autor: Ramiro Matias Sanchez (RAMIRO) ramiromatias@gmail.com
 % Fecha: 06/07/21
-% Sesión: Examen de recuperacion
+% SesiÃ³n: Examen de recuperacion
 % ejercicio 1
 
-clc
-clear
 syms x;
-% Funcion tipo = 2x^2+x^2-6
-fx = input ("Coloque la funcion:")
-funcion = inline(char (fx))
-% Establecer los limites 
-a = input ("ingresar el valor de a")
-b = input ("ingrasar el valor de b")
-% Operaciones 
-resultado = ((b ^ 3) / 3 - b ^ 2 + 3 * b) - ((a ^ 3) / 3 - a ^ 2 + 3 * a)
-% Rango 
-x = 1: 0,2: 2;
-f = x;
-trama (f, x)
-barra (f, x, "m")
+f = ((2)*(x^2)+((x.^2)-6)
+a = 1;
+b = 7;
+int(f, a, b)
+ezplot(f)
